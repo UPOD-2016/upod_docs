eaf9947ceb399d1d97f3e7de0ac9aa38d33dcb39
rmoro <moro1422@mylaurier.ca>
2016-06-01 16:26:34 -0400

    Site is ready to be deployed with current frontend skeleton.  YOU MUST RUN bundle install then UPDATE config/database.yml and RUN rake db:create then RUN rails s to use

--- upod/config/routes.rb
+++ upod/config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
    root 'home#index'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+     get 'home' => 'home#index'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
