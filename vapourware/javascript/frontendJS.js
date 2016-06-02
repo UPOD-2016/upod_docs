22483bfcd7841fd6d8c649d0503eb6c474a20536
Vik Sharma <shar6770@mylaurier.ca>
2016-06-02 12:29:01 -0400

    add push menu to search-result.html, reorganize and add push menu to header

--- vapourware/javascript/frontendJS.js
+++ vapourware/javascript/frontendJS.js
@@ -1,6 +1,26 @@
 (function() {
-    console.log("hello");
-    $("#headerContent").load("header.html"); // load header/nav-bar
+    $("#headerContent").load("header.html", function(){
+        $('.icon-menu').click(function() {
+            $('.menu').animate({
+                left: "0px"
+            }, 200);
+
+            $('body').animate({
+                left: "285px"
+            }, 200);
+            console.log("hello");
+        });
+
+        $('.icon-close').click(function() {
+            $('.menu').animate({
+                left: "-285px"
+            }, 200);
+
+            $('body').animate({
+                left: "0px"
+            }, 200);    
+        });        
+    }); // load header/nav-bar
     $("#footerContent").load("footer.html"); // load footer
     $("#pushmenuContent").load("push-menu.html", function() {
         $('.icon-menu').click(function() {
