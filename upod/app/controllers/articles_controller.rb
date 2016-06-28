bdaa693ae49ab3a0c16ed128f1da76a3487eb82d
Mike Roher <roher4@gmail.com>
2016-06-28 15:49:25 +0000

    starting to integrate backend

--- upod/app/controllers/articles_controller.rb
+++ upod/app/controllers/articles_controller.rb
@@ -21,13 +21,7 @@ class ArticlesController < ApplicationController
 
   # POST /articles
   def create
-    @article = Article.new(article_params)
 
-    if @article.save
-      redirect_to @article, notice: 'Article was successfully created.'
-    else
-      render :new
-    end
   end
 
   # PATCH/PUT /articles/1
