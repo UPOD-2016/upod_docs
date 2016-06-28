00d2ecc44b32521ec597e4a2537275e1c3db4149
Kieran O'Driscoll <oxdr1700@mylaurier.ca>
2016-06-28 02:55:23 +0000

    Add comments for RSpec/Shoulda matcher tests

--- upod/spec/models/article_text_block_spec.rb
+++ upod/spec/models/article_text_block_spec.rb
@@ -1,8 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe ArticleTextBlock, type: :model do
+
+
+# uses shoulda matcher as well as RSpec to validate and test the presence
+# of the body of an ArticleTextBlock
   it { is_expected.to validate_presence_of(:body) }
 
+
+# uses shoulda matcher as well as RSpec to validate and test the length
+# of the body of an ArticleTextBlock
   it do
     should validate_length_of(:body).
       is_at_most(65535).
