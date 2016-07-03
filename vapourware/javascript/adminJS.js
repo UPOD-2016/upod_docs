dfd1110163793b5ad2baa3e5682babf61577a7af
tudorbertiean <tudorbertiean@hotmail.com>
2016-07-03 15:56:34 -0400

    added jquery to admin menu

---
+++ vapourware/javascript/adminJS.js
@@ -0,0 +1,7 @@
+$('#new-category').on('click',function(){
+    $("#dynamic-header").html("You are now adding a new category");
+});
+
+$('.panel-footer').on('click',function(){
+    $("#dynamic-header").html("You are now adding a new sub-category");
+});
