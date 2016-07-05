213d49f300bdacd855b0cbbb9bdcaa5cefe2dd6f
Ryan <ryan1110mak@yahoo.ca>
2016-07-05 14:02:50 -0400

    Fixed an issue where the header wouldn't show on all pages
    The JS code where it loads the header was accidentally commented out.

--- vapourware/javascript/frontendJS.js
+++ vapourware/javascript/frontendJS.js
@@ -1,6 +1,30 @@
 var main = function() {
     $("#footerContent").load("footer.html"); // load footer
     $("#slideMenuContent").load("slideMenu.html"); // load slide menu    
+	
+	$("#headerContent").load("header.html", function(){
+        $('.icon-menu').click(function() {
+            $('.menu').animate({
+                left: "0px"
+            }, 200);
+
+            $('body').animate({
+                left: "285px"
+            }, 200);
+            console.log("hello");
+        });
+
+        $('.icon-close').click(function() {
+            $('.menu').animate({
+                left: "-285px"
+            }, 200);
+
+            $('body').animate({
+                left: "0px"
+            }, 200);    
+        });        
+    }); // load header/nav-bar
+	
 };
 $(document).ready(main);
 /* using new slide menu so dont need below code
