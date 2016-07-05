878ab0379054f05cc0b0057eef423359ca9c0066
Vik Sharma <shar6770@mylaurier.ca>
2016-07-05 16:38:50 -0400

    update frontendJS.js (redundant lines)

--- vapourware/javascript/frontendJS.js
+++ vapourware/javascript/frontendJS.js
@@ -1,29 +1,7 @@
 var main = function() {
     $("#footerContent").load("footer.html"); // load footer
     $("#slideMenuContent").load("slideMenu.html"); // load slide menu    
-	
-	$("#headerContent").load("header.html", function(){
-        $('.icon-menu').click(function() {
-            $('.menu').animate({
-                left: "0px"
-            }, 200);
-
-            $('body').animate({
-                left: "285px"
-            }, 200);
-            console.log("hello");
-        });
-
-        $('.icon-close').click(function() {
-            $('.menu').animate({
-                left: "-285px"
-            }, 200);
-
-            $('body').animate({
-                left: "0px"
-            }, 200);    
-        });        
-    }); // load header/nav-bar
+	$("#headerContent").load("header.html"); // load header/nav-bar
 	
 };
 $(document).ready(main);
