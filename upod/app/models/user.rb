d1853b5d03c8a64357927c5e1c990a9af3042a01
rmoro <moro1422@mylaurier.ca>
2016-07-06 19:19:19 -0400

    adding session table

--- upod/app/models/user.rb
+++ upod/app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :ldap_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  def ldap_before_save
+   self.email = Devise::LDAP::Adapter.get_ldap_param(self.username,"mail").first
+  end
 end
