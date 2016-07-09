9cafd120bedf19b07338465f57e80833434c24ff
chan6240 <chan6240@mylaurier.ca>
2016-07-09 06:37:45 +0000

    Show all categories

--- upod/app/controllers/categories_controller.rb
+++ upod/app/controllers/categories_controller.rb
@@ -2,6 +2,9 @@ class CategoriesController < ApplicationController
   def new
   end
 
+  def index
+    @categories = Category.all
+  end
   def show
     @category = Category.find(params[:id])
   end
