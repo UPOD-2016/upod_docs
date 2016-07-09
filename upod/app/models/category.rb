99954aba4fb3fb4a4db1a9ea7d7153faa6f6352f
chan6240 <chan6240@mylaurier.ca>
2016-07-09 05:45:16 +0000

    Category model

---
+++ upod/app/models/category.rb
@@ -0,0 +1,5 @@
+class Category < ActiveRecord::Base
+  before_save { self.name = (name.downcase).titleize }
+  validates :name, presence: true, length: {maximum: 255}
+                 ,uniqueness: { case_sensitive: false }
+end
