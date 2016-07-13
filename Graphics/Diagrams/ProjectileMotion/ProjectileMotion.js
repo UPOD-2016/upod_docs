f9697c0d4d51bca70ecfd8ae75f6020e7d8e7670
Roman Lipisiy <lipi4200@mylaurier.ca>
2016-07-13 12:37:02 -0400

    fix button cross browser, add max height constraint

--- Graphics/Diagrams/ProjectileMotion/ProjectileMotion.js
+++ Graphics/Diagrams/ProjectileMotion/ProjectileMotion.js
@@ -9,16 +9,16 @@ var start = document.getElementById("start");
 var mid = document.getElementById("mid");
 var end = document.getElementById("end");
 var EndText = document.getElementById("EndText");
+var MaxHeight = 0;
 
-
-function reset(){
+function reset() {
     curve.setAttribute("opacity", "0.2");
     start.setAttribute("opacity", "0");
     mid.setAttribute("opacity", "0");
     end.setAttribute("opacity", "0");
 };
 
-function launch(){
+function launch() {
     curve.setAttribute("opacity", "1");
     start.setAttribute("opacity", "1");
     mid.setAttribute("opacity", "1");
@@ -27,18 +27,18 @@ function launch(){
 };
 
 function calculate() {
-    var theta = LaunchAngle*Math.PI/180;
-    var MaxHeight = Math.sin(theta)*Math.sin(theta)*vi*vi/(2*9.8);
-    var TimeOfFlight = 2*Math.sin(theta)*vi/9.8;
-    var Distance = vi*vi*Math.sin(2*theta)/9.8;
-	
-	
-    var X1 = ((Distance.toFixed(0)/2)+100);
-    var X2 = ((Distance.toFixed(0)/1)+100);
-    var Y  = (500-2*MaxHeight.toFixed(0));
-    var path = "M 100 500 Q " + X1+" "+Y+" "+X2+" 500";
+    var theta = LaunchAngle * Math.PI / 180;
+    MaxHeight = Math.sin(theta) * Math.sin(theta) * vi * vi / (2 * 9.8);
+    var TimeOfFlight = 2 * Math.sin(theta) * vi / 9.8;
+    var Distance = vi * vi * Math.sin(2 * theta) / 9.8;
+
+
+    var X1 = ((Distance.toFixed(0) / 2) + 100);
+    var X2 = ((Distance.toFixed(0) / 1) + 100);
+    var Y = (500 - 2 * MaxHeight.toFixed(0));
+    var path = "M 100 500 Q " + X1 + " " + Y + " " + X2 + " 500";
     //curve.style.stroke-dasharray = curve.getTotalLength();
-    
+
     TimeOut.textContent = TimeOfFlight.toFixed(2);
     //TimeOut.textContent = curve.getTotalLength();
 
@@ -48,7 +48,7 @@ function calculate() {
 
     curve.setAttribute("d", path);
     mid.setAttribute("cx", X1);
-    mid.setAttribute("cy", (500-MaxHeight.toFixed(0)));
+    mid.setAttribute("cy", (500 - MaxHeight.toFixed(0)));
     end.setAttribute("cx", X2);
     EndText.setAttribute("x", X2);
 };
@@ -57,46 +57,69 @@ function calculate() {
 //    example.setAttribute("Stroke","Black");
 //    $ball.velocity({cy: "200",}, {duration: 300,easing: "linear"});
 //};
-function IncreaseAngle(){
-    var angle = parseInt(LaunchAngle);
-    reset();
-    if (angle<85){
-	angle +=5;
-	document.getElementById("LaunchAngle").textContent = angle;
-	LaunchAngle = angle;
-	calculate();
+//
+function checkHeight() {
+    if (MaxHeight > 300)
+        return "false";
+    else {
+        return "true"
     }
 };
 
-function DecreaseAngle(){
+function IncreaseAngle() {
     var angle = parseInt(LaunchAngle);
     reset();
-    if (angle>15){
-        angle -=5;
+    if (angle < 85) {
+        angle += 5;
         document.getElementById("LaunchAngle").textContent = angle;
         LaunchAngle = angle;
-	calculate();
+        calculate();
+        var bool = checkHeight();
+        if (bool == "false") {
+          angle -= 5;
+          document.getElementById("LaunchAngle").textContent = angle;
+          LaunchAngle = angle;
+          calculate();
+        }
     }
 };
 
-function IncreaseVelocity(){
-    var velocity = parseInt(vi);
+function DecreaseAngle() {
+    var angle = parseInt(LaunchAngle);
     reset();
-    if (velocity<100){
-        velocity +=5;
-        document.getElementById("vi").textContent = velocity;
-        vi = velocity;
-	calculate();
+    if (angle > 15) {
+        angle -= 5;
+        document.getElementById("LaunchAngle").textContent = angle;
+        LaunchAngle = angle;
+        calculate();
     }
 };
 
-function DecreaseVelocity(){
+function IncreaseVelocity() {
     var velocity = parseInt(vi);
     reset();
-    if (velocity>20){
-        velocity -=5;
+    if (velocity < 100) {
+        velocity += 5;
         document.getElementById("vi").textContent = velocity;
         vi = velocity;
-	calculate();
+        calculate();
+        var bool = checkHeight();
+        if (bool == "false") {
+          velocity -= 5;
+          document.getElementById("vi").textContent = velocity;
+          vi = velocity;
+          calculate();
+        }
+    }
+};
+
+function DecreaseVelocity() {
+    var velocity = parseInt(vi);
+    reset();
+    if (velocity > 20) {
+        velocity -= 5;
+        document.getElementById("vi").textContent = velocity;
+        vi = velocity;
+        calculate();
     }
 };
