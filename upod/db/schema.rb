a0666356b7c700206402a9dfa9b5492e829583bb
Mike Roher <roher4@gmail.com>
2016-07-21 16:47:26 +0000

    Merging with Kieran

--- upod/db/schema.rb
+++ upod/db/schema.rb
@@ -58,7 +58,10 @@ ActiveRecord::Schema.define(version: 20160721163639) do
     t.string   "title",      limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+    t.string   "slug",       limit: 255
   end
+
+  add_index "articles", ["slug"], name: "index_articles_on_slug", using: :btree
 
   create_table "categories", force: :cascade do |t|
     t.string   "name",        limit: 255
@@ -107,6 +110,19 @@ ActiveRecord::Schema.define(version: 20160721163639) do
   end
 
   add_index "equation_block_variables", ["article_equation_block_id"], name: "index_equation_block_variables_on_article_equation_block_id", using: :btree
+
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string   "slug",           limit: 255, null: false
+    t.integer  "sluggable_id",   limit: 4,   null: false
+    t.string   "sluggable_type", limit: 50
+    t.string   "scope",          limit: 255
+    t.datetime "created_at"
+  end
+
+  add_index "friendly_id_slugs", ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true, using: :btree
+  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", using: :btree
+  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
+  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
 
   create_table "images", force: :cascade do |t|
     t.text     "body",              limit: 65535
