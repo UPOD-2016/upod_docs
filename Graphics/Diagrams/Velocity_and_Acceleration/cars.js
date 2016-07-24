84824a0210ce6077173c7ab507b757b8dcbf56d1
Joseph <vann7860@mylaurier.ca>
2016-07-24 18:08:12 -0400

    Animate the other two cars, animate a light to start the race and add buttons to start and reset the animation of the cars

--- Graphics/Diagrams/Velocity_and_Acceleration/cars.js
+++ Graphics/Diagrams/Velocity_and_Acceleration/cars.js
@@ -1,7 +1,85 @@
 var $element = $("#car");
+var $element2 = $("#car2");
+var $element3 = $("#car3");
+var $light1 = $("#l1");
+var $light2 = $("#l2");
+var $light3 = $("#l3");
+var light1=document.getElementById("l1");
+var light2=document.getElementById("l2");
+var light3=document.getElementById("l3");
 
 
+var tracker=0;
+
+//animates the lights
+function lights() {
+
+$light1.velocity({
+    opacity: 1
+    
+}, {
+    duration: 1000
+});
+
+setTimeout(function() {
+    $light2.velocity({
+      opacity: 1
+    }, {
+      queue: false,
+      duration: 1000
+    });
+}, 1000);
+
+
+setTimeout(function() {
+    $light3.velocity({
+      opacity: 1
+    }, {
+      queue: false,
+      duration: 1000
+    });
+}, 2000);
+
+}
+
+
+//animates the three cars with different rates of acceleration
 function animate() {
+
+setTimeout(function() {
+	$element.velocity({
+		translateX: "90000px"
+	
+	}, {
+		duration: 1000,
+		easing: "easeInSine",
+	});
+}, 2500);	
+
+setTimeout(function() {
+	$element2.velocity({
+		translateX: "90000px"
+	
+	}, {
+		duration: 1000,
+		easing: "linear",
+	});
+}, 2500);	
+
+setTimeout(function() {
+	$element3.velocity({
+		translateX: "90000px"
+	
+	}, {
+		duration: 1000,
+		easing: "easeOutSine",
+	});
+}, 2500);	
+
+}
+
+//sets the cars in their starting positions
+function redo() {
 
 
 $element.velocity({
@@ -16,16 +94,48 @@ $element.velocity({
     
 });
 
-$element.velocity({
-    translateX: "90000px"
+$element2.velocity({
+    scaleX: "0.01000",
+	scaleY: "-0.01000",
+	translateX: "7000px",
+	translateY: "-15000px"
     
 }, {
-    duration: 1000,
-    easing: "easeInSine",
+    duration: 0,
+    easing: "linear"
+    
 });
 
+$element3.velocity({
+    scaleX: "0.01000",
+	scaleY: "-0.01000",
+	translateX: "7000px",
+	translateY: "-22000px"
+    
+}, {
+    duration: 0,
+    easing: "linear"
+    
+});
 
 }
 
 
-animate();
+//calls the functions to animate the cars when the start race button is pushed
+function start(){
+	if(tracker==0){
+		lights();
+		redo();
+		animate();
+	}
+	tracker=1;
+};
+
+//calls the function to reset the cars when the reset button is pushed
+function reset(){
+	tracker=0;
+	redo();
+	light1.style.opacity = "0.0";
+	light2.style.opacity = "0.0";
+	light3.style.opacity = "0.0";
+};
--- Graphics/Diagrams/Velocity_and_Acceleration/readme.txt
+++ Graphics/Diagrams/Velocity_and_Acceleration/readme.txt
@@ -2,10 +2,11 @@ Name: Joseph Vannelli
 
 Diagram: Velocity and Acceleration Car Diagram
 
-Status: I have been able to animate the movement of one of the cars, and now I am working on getting the other two cars to move at the same time as the first one.
+Status: I have animated the three cars and now I just have to add in a description about the acceleration and velocity of each of the cars
 
 File descriptions: 
      1. cars.html: This file creates the images for the cars
      2. cars.js: This file uses velocity to animate the movement of the first car
+     3. style.css: This file contains all of the styling for the buttons
 
 Demo: http://hopper.wlu.ca/~vann7860/velocity_and_acceleration/cars.html
